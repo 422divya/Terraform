1751371e13d2994c4159abc6052d246fb3752077
root <root@ip-172-31-15-105.ap-northeast-1.compute.internal>
2023-04-11 06:40:10 +0000

    modified nginx-instance code

--- terraform-ngnix-instance/ngnix-instance.tf
+++ terraform-ngnix-instance/ngnix-instance.tf
@@ -16,7 +16,7 @@ resource "docker_image" "nginx"  {
 }
 
 resource "docker_container" "container" {
-  image = docker_image.nginx.latest
+  image = docker_image.nginx.image_id
   name = "nginx-container"
   ports {
      internal = 80
