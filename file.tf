b0f301939a3537952e076f82c665164b0ab43e9d
Divya Prabhu <85998740+422divya@users.noreply.github.com>
2023-07-04 12:41:32 +0530

    Create file.tf

---
+++ file.tf
@@ -0,0 +1,33 @@
+terraform {
+       required_providers {
+           aws = {
+              source = "hashicorp/aws"
+              version = "5.1.0"
+ 
+}
+}
+
+}
+
+
+provider "aws" {
+   region = "ap-northeast-1"
+
+}
+
+
+resource "aws_instance" "terraform-instance" {
+         ami = "ami-03dceaabddff8067e"
+         instance_type = "t1.micro"
+         tags = {
+              Name = "lifecycle"
+}
+
+#lifecycle {
+#   ignore_changes = [
+#      tags,
+#      instance_type
+#  ]
+
+# }
+} 
